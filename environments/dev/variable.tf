4d7024e3ec3f872552c301ad696898c9bcbb59d3
vijendra Singh <vijendra.devops@gmail.com>
2025-06-01 13:36:22 +0530

    infra code added

--- environments/dev/variable.tf
+++ environments/dev/variable.tf
@@ -1,10 +1,10 @@
 variable "dev-rgs" {}
-# variable "dev-stgs" {}
-# variable "dev-cons" {}
-# variable "dev-vnets" {}
+variable "dev-stgs" {}
+variable "dev-cons" {}
+variable "dev-vnets" {}
 # # variable "dev-win-vms" {}
-# variable "dev-linux-vms" {}
-# variable "dev-nsgs" {}
-# variable "dev-asgs" {}
+variable "dev-linux-vms" {}
+variable "dev-nsgs" {}
+variable "dev-asgs" {}
 # variable "dev-bastions" {}
-# variable "dev-keyvaults" {}
+variable "dev-keyvaults" {}
